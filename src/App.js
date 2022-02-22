3028122666bccfdb1e2cc075b965185ef44901ea
amaiYume <72297102+amaiYume@users.noreply.github.com>
2022-02-22 21:17:06 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './screens/registration/registraion'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Registration from './screens/registration/registraion';
-import firebase from 'firebase/compat/app'
 import Login from './screens/login/login';
+import ChangePass from './screens/changepassword/changepass';
+import Mainlibrary from './screens/MainLibrary/Mainlibrary';
 
 function App() {
-  const firebaseApp = firebase.apps[0];
   return (
     <div className="App">
        <h1>Black Box</h1>
-      
-   
-      <Registration/>
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Registration/>}  exact />
+          <Route path = "/login" element = {<Login/>} exact />
+          <Route path = "/changepassword" element = {<ChangePass/>} exact />
+          <Route path = "/Mainlibrary" element = {<Mainlibrary />} exact />
+        </Routes>
+      </Router>
     </div>
   );
 }

874186eba32ffb8b2d6f7777bf66c26d68fa26bf
amaiYume <72297102+amaiYume@users.noreply.github.com>
2022-03-06 17:55:57 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Login from './screens/login/login';
-import Registration from './screens/registration/registraion';
-
-
 ReactDOM.render(
    
 <App/>

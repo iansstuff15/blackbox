03008eed6cf9cc05fb63d719b0e637a0ec479e05
amaiyume <zehlljael@gmail.com>
2022-03-07 19:59:01 +0800

    deploy

--- server.js
+++ server.js
@@ -12,117 +12,117 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/games/:game_id', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/games/${req.params.game_id}?key=${process.env.MY_API_KEY}`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data)
-        console.log( res.json(response.data))
-    }).catch(error =>console.log(error))
-})
+// app.get('/games/:game_id', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/games/${req.params.game_id}?key=${process.env.MY_API_KEY}`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data)
+//         console.log( res.json(response.data))
+//     }).catch(error =>console.log(error))
+// })
 
-app.get('/games/:game_id/screenshots', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/screenshots?key=${process.env.MY_API_KEY}`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-        console.log( res.json(response.data))
-    }).catch(error =>console.log(error))
-})
-
-
-app.get('/games/:game_id/movies', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/movies?key=${process.env.MY_API_KEY}`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-        console.log( res.json(response.data))
-    }).catch(error =>console.log(error))
-})
+// app.get('/games/:game_id/screenshots', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/screenshots?key=${process.env.MY_API_KEY}`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//         console.log( res.json(response.data))
+//     }).catch(error =>console.log(error))
+// })
 
 
-app.get('/games/:game_id/achievements', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/achievements?key=${process.env.MY_API_KEY}`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-        console.log( res.json(response.data))
-    }).catch(error =>console.log(error))
-})
+// app.get('/games/:game_id/movies', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/movies?key=${process.env.MY_API_KEY}`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//         console.log( res.json(response.data))
+//     }).catch(error =>console.log(error))
+// })
 
-app.get('/games/genre/:genre', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+// app.get('/games/:game_id/achievements', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/games/${req.params.game_id}/achievements?key=${process.env.MY_API_KEY}`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//         console.log( res.json(response.data))
+//     }).catch(error =>console.log(error))
+// })
+
+// app.get('/games/genre/:genre', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     
-    axios.get(`https://api.rawg.io/api/games?key=${process.env.MY_API_KEY}&genres=${req.params.genre.toLowerCase()}`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-    }).catch(error =>console.log(error))
-})
+//     axios.get(`https://api.rawg.io/api/games?key=${process.env.MY_API_KEY}&genres=${req.params.genre.toLowerCase()}`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//     }).catch(error =>console.log(error))
+// })
 
-app.get('/games', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/games?key=${process.env.MY_API_KEY}&page_size=10`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-    }).catch(error =>console.log(error))
-})
+// app.get('/games', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/games?key=${process.env.MY_API_KEY}&page_size=10`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//     }).catch(error =>console.log(error))
+// })
 
-app.get('/genre', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    axios.get(`https://api.rawg.io/api/genres?key=${process.env.MY_API_KEY}&count=1`,
-    {
-    headers:{
-        'Content-Type': 'application/json',
-    } 
-    }
-    ).then(response => {
-        //the game library appears here and assign codes using res.data.result
-        res.json(response.data.results)
-    }).catch(error =>console.log(error))
-})
+// app.get('/genre', (req, res) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     axios.get(`https://api.rawg.io/api/genres?key=${process.env.MY_API_KEY}&count=1`,
+//     {
+//     headers:{
+//         'Content-Type': 'application/json',
+//     } 
+//     }
+//     ).then(response => {
+//         //the game library appears here and assign codes using res.data.result
+//         res.json(response.data.results)
+//     }).catch(error =>console.log(error))
+// })
 
 
 

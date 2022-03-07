87f891c3237b95e90445e5cff1a4fec035c41635
amaiYume <zehlljael@gmail.com>
2022-03-08 01:01:02 +0800

    lol

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 const path = require('path');
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'Client/build')))
 
 
 app.get('/', (req, res) => {
@@ -127,8 +129,7 @@ app.get('/genre', (req, res) => {
 })
 
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'Client/build')))
+
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/Client/build/index.html'))

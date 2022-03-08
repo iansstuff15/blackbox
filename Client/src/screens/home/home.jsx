487bd143f99d2c64194502377e0bd4e510be22e8
amaiYume <zehlljael@gmail.com>
2022-03-08 13:40:15 +0800

    please

--- Client/src/screens/home/home.jsx
+++ Client/src/screens/home/home.jsx
@@ -19,7 +19,7 @@ class Home extends Component {
       
       fetchUpcoming() {
 
-                axios.get('https://secret-depths-46783.herokuapp.com/games',
+                axios.get('https://secret-depths-46783.herokuapp.com/games' || `http://localhost:8000/games`,
                     {
                     headers:{
                         'Content-Type': 'application/json',
@@ -32,7 +32,7 @@ class Home extends Component {
                     this.setState({games:res.data})
                 }).catch(error =>console.log(error))
 
-                axios.get(`https://secret-depths-46783.herokuapp.com/genre`,
+                axios.get(`https://secret-depths-46783.herokuapp.com/genre` || `http://localhost:8000/genre`,
                         {
                         headers:{
                             'Content-Type': 'application/json',

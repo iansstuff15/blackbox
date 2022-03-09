9a02b5699301b15e81c878fa8d78b213e45a665d
amaiYume <zehlljael@gmail.com>
2022-03-10 01:28:39 +0800

    updated to render gamecard

--- Client/src/screens/library/library.jsx
+++ Client/src/screens/library/library.jsx
@@ -1,53 +1,54 @@
 import React, {useState, useEffect} from 'react'
 import { handleGetSnapshotOfLibraryContent } from '../../helper/firebase_get'
 import './library.css';
-import {useNavigate} from 'react-router-dom';
+import '../home/home.css'
+import GameCard from '../../components/game-card/game_card'
+// import {useNavigate} from 'react-router-dom';
 import axios from 'axios';
 // import firebase from '../../firebase/firebase';
 export default function Library() {
-        let navigate = useNavigate()
-        const[Library,setLibrary] = useState();
-        useEffect(()=>{
-            handleGetSnapshotOfLibraryContent().then(data=>setLibrary(data['0']))
 
-            // firebase.database().ref('users').on('value', (snapshot)=> {
-            //     const library = snapshot.val();
-            //     const getDatabase = [];   
-            //     for (let id in library){
-            //         // if(id === "vYENHq0syIY1ssVjuK3v13bS3Nx2")
-            //         getDatabase.push(library[id])
-            //     }
-            //     setlibrary(getDatabase)
-            //     console.log(getDatabase)
-            // })
-           
-        },[]);
+        const[games, setGames] = useState([]);
 
-        const fetchUpcoming =  (gameid) =>{
+        const fetchForDesign = (gameid) =>{
             return axios.get(`https://blackbox-heroku.herokuapp.com/games/${gameid}` || `http://localhost:8000/games/${gameid}`,
-                    {
-                    headers:{
-                        'Content-Type': 'application/json',
-                        
-                    } 
-                    }
-                ).then(res => {
-                    //the game library appears here and assign codes using res.data.result
-                    navigate(`/game/${gameid}`, {state:res.data})
-                }).catch(error =>console.log(error))
+            {
+            headers:{
+                'Content-Type': 'application/json',
+                
+            } 
+            }
+        ).then(res => {
+            //the game library appears here and assign codes using res.data.result
+            return  res.data
+        }).catch(error =>console.log(error))
         }
 
+        useEffect(()=>{
+            handleGetSnapshotOfLibraryContent().then(data=> {
+            Object.keys(data['0']).map((gameName)=>{
+                var name = JSON.stringify(gameName)
+                var newname = name.substring(1, name.length-1)
+                const gameid = data['0'][newname]['data']['gameId']
+                fetchForDesign(gameid).then(data => {setGames((prevState)=>([...prevState, data])) }
+                )
+                return null
+            })
+        })
+
+        },[]);  
 
         return(
-            <div>
-                
-                {Library?Object.keys(Library).map((gameName)=>{
-                    var name = JSON.stringify(gameName)
-                    var newname = name.substring(1, name.length-1)
-                    const gameid = Library[newname]['data']['gameId']
-                    
-                    return (<h1 key = {name} onClick = {()=>{fetchUpcoming(gameid)}}>{newname}</h1>)
-                }) : <p>Loading</p>}
+            <div className = "home">
+                <h1>Game Library</h1>
+                 {
+                    games?games.map((game) => (
+                    <span key = {game.id}>
+                    <GameCard gameName={game.name} gameIMG={game.background_image} gameReleased={game.released} gameData={game}/>           
+                    </span>
+                    )):<p>loading</p>
+                    && setGames()
+                }
             </div>
         )
     
